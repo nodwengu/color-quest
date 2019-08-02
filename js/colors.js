a2d98d821844f3ac9059baf0f60fc5ff86675cc3
Thando <nodwengut@gmail.com>
2019-08-02 15:51:59 +0200

    added the filter

--- js/colors.js
+++ js/colors.js
@@ -12,10 +12,80 @@ function ColorQuestManager() {
 	function getShapes() {
 		return shapes
 	}
+
+	// function filter(filterParams) {
+	// 	return [ 
+	// 		{color: 'blue', number: 7},
+	// 		{color: 'blue', number: 8} 
+	// 	]
+	// }
+
+	// function filter(filterParams) {
+	// 	// return [ 
+	// 	// 	{color: 'yellow', number: 3},
+    //     //     {color: 'yellow', number: 6} 
+	// 	// ]
+	// 	const filterColor = filterParams.color;
+	// 	const filteredShapes = [];
+	// 	//Loop over all shapes
+	// 	for(let i = 0; i < shapes.length; i++) {
+	// 		let currentShape = shapes[i];
+	// 		//check if the current shape has a color we are looking for
+	// 		if(currentShape.color === filterColor) {
+	// 			filteredShapes.push(currentShape);
+	// 		}
+	// 	}
+
+	// 	return filteredShapes;
+
+	// }
+
+	function filter(filterParams) {
+		// return [ 
+		// 	{color: 'yellow', number: 3},
+        //     {color: 'yellow', number: 6} 
+		// ]
+		const filterColor = filterParams.color;
+		const filterNumber = filterParams.number;
+
+		const filteredShapes = [];
+		//Loop over all shapes
+		if(filterColor !== undefined && filterNumber !== undefined) {
+			for(let i = 0; i < shapes.length; i++) {
+				let currentShape = shapes[i];
+				//check if the current shape has a color we are looking for
+				if(currentShape.color === filterColor && currentShape.number === filterNumber) {
+					filteredShapes.push(currentShape);
+				}
+			}
+
+		} else if(filterColor !== undefined) {
+			for(let i = 0; i < shapes.length; i++) {
+				let currentShape = shapes[i];
+				//check if the current shape has a color we are looking for
+				if(currentShape.color === filterColor) {
+					filteredShapes.push(currentShape);
+				}
+			}
+		} else if(filterNumber !== undefined) {
+			for(let i = 0; i < shapes.length; i++) {
+				let currentShape = shapes[i];
+				//check if the current shape has a color we are looking for
+				if(currentShape.number === filterNumber) {
+					filteredShapes.push(currentShape);
+				}
+			}
+		} 
+
+		return filteredShapes;
+
+	}
+	
 	
 
 	return {
 		addShape,
 		getShapes,
+		filter
 	}
 }
--- test/colors.test.js
+++ test/colors.test.js
@@ -7,37 +7,170 @@ describe("The ColorsQuest Factory Function", function() {
         colorQuestManager.addShape("orange", 3);
       
         assert.equal(2, colorQuestManager.getShapes().length);
+    });
 
-        //Checking
-        const shapeOne = colorQuestManager.getShapes()[0];
-        assert.equal("blue", colorQuestManager.shapeOne.color);
-        // assert.equal(7, colorQuestManager.shapeOne.number);
+    it('Should allow me to add 3 shapes', function(){
+        const colorQuestManager = ColorQuestManager();
 
-        // const shapeTwo = colorQuestManager.getShapes()[1];
-        // assert.equal("orange", colorQuestManager.shapeTwo.color);
-        // assert.equal(3, colorQuestManager.shapeTwo.number);
+        colorQuestManager.addShape("blue", 7);
+        colorQuestManager.addShape("orange", 3);
+        colorQuestManager.addShape("orange", 8);
+      
+        const shapeList = colorQuestManager.getShapes();
+        assert.equal(3, shapeList.length);
       
     });
 
-    it('Should allow me to add 2 shapes', function(){
+    it('Should be able to find all blue shapes', function(){
         
-        const colorQuestManager = colorQuestManager();
+        const colorQuestManager = ColorQuestManager();
 
-        colorQuestManager.addShapes("blue", 7);
-        colorQuestManager.addShapes("orange", 3);
-      
-        assert.equal(2, colorQuestManager.getShapes());
+        //Add some shapes
+        colorQuestManager.addShape("blue", 7);
+        colorQuestManager.addShape("yellow", 3);
+        colorQuestManager.addShape("blue", 8);
+        colorQuestManager.addShape("orange", 8);
 
-        //Checking
-        const shapeOne = colorQuestManager.getShapes()[0];
-        assert.equal("blue", colorQuestManager.shapeOne.color);
-        assert.equal(7, colorQuestManager.shapeOne.number);
+        //Act
+        //filter shapes
+        const filteredShapes = colorQuestManager.filter({
+            color: 'blue'
+        })
 
-        const shapeTwo = colorQuestManager.getShapes()[1];
-        assert.equal("orange", colorQuestManager.shapeTwo.color);
-        assert.equal(3, colorQuestManager.shapeTwo.number);
+        //Checking that the right number of shapes was returned
+        assert.equal(2, filteredShapes.length);
+
+        assert.deepEqual([
+            {color: 'blue', number: 7},
+            {color: 'blue', number: 8}],
+            filteredShapes
+        );
       
     });
+
+    it('Should be able to find all yellow shapes', function(){
+        
+        const colorQuestManager = ColorQuestManager();
+
+        //Add some shapes
+        colorQuestManager.addShape("blue", 7);
+        colorQuestManager.addShape("yellow", 3);
+        colorQuestManager.addShape("blue", 8);
+        colorQuestManager.addShape("orange", 8);
+        colorQuestManager.addShape("yellow", 6);
+
+        //Act
+
+        //filter shapes
+        const params = {
+            color: 'yellow'
+        }
+        const filteredShapes = colorQuestManager.filter(params)
+
+        //Checking that the right number of shapes was returned
+        assert.equal(2, filteredShapes.length);
+      
+        assert.deepEqual([
+            {color: 'yellow', number: 3},
+            {color: 'yellow', number: 6}],
+            filteredShapes
+        );
+      
+    });
+
+    it('Should be able to find all the number shapes', function(){
+        
+        const colorQuestManager = ColorQuestManager();
+
+        //Add some shapes
+        colorQuestManager.addShape("orange", 3);
+        colorQuestManager.addShape("yellow", 3);
+        colorQuestManager.addShape("blue", 8);
+        colorQuestManager.addShape("orange", 8);
+        colorQuestManager.addShape("yellow", 6);
+
+        //Act
+
+        //filter shapes
+        const params = {
+            number: 3
+        }
+        const filteredShapes = colorQuestManager.filter(params)
+
+        //Checking that the right number of shapes was returned
+        assert.equal(2, filteredShapes.length);
+      
+        assert.deepEqual([
+            {color: 'orange', number: 3},
+            {color: 'yellow', number: 3}],
+            filteredShapes
+        );
+      
+    });
+
+    it('Should be able to find all the number shapes', function(){
+        
+        const colorQuestManager = ColorQuestManager();
+
+        //Add some shapes
+        colorQuestManager.addShape("orange", 7);
+        colorQuestManager.addShape("yellow", 4);
+        colorQuestManager.addShape("blue", 7);
+        colorQuestManager.addShape("orange", 8);
+        colorQuestManager.addShape("yellow", 6);
+
+        //Act
+
+        //filter shapes
+        const params = {
+            number: 7
+        }
+        const filteredShapes = colorQuestManager.filter(params)
+
+        //Checking that the right number of shapes was returned
+        assert.equal(2, filteredShapes.length);
+      
+        assert.deepEqual([
+            {color: 'blue', number: 7},
+            {color: 'orange', number: 7}],
+            filteredShapes
+        );
+      
+    });
+
+    it('Should be able to find all the orange number 7 shapes', function(){
+        
+        const colorQuestManager = ColorQuestManager();
+
+        //Add some shapes
+        colorQuestManager.addShape("orange", 7);
+        colorQuestManager.addShape("yellow", 4);
+        colorQuestManager.addShape("blue", 7);
+        colorQuestManager.addShape("orange", 7);
+        colorQuestManager.addShape("orange", 7);
+        colorQuestManager.addShape("yellow", 6);
+
+        //Act
+
+        //filter shapes
+        const params = {
+            number: 7,
+            color: 'orange'
+        }
+        const filteredShapes = colorQuestManager.filter(params)
+
+        //Checking that the right number of shapes was returned
+        assert.equal(3, filteredShapes.length);
+      
+        assert.deepEqual([
+            {color: 'orange', number: 7},
+            {color: 'orange', number: 7},
+            {color: 'orange', number: 7}],
+            filteredShapes
+        );
+      
+    });
+
 
     
 });
